833135fc40a1e75bff409accfb96cd5e1228aca4
VENOM <117318674+VENOM7XxX@users.noreply.github.com>
2023-02-24 12:39:04 +0200

    Update NASA.lua

--- NASA.lua
+++ NASA.lua
@@ -217,7 +217,7 @@ end
 
 if Dev(msg) then
 if text == "الاصدار" or text == "الاصدار ‌⚙️⁩" then
-LuaTele.sendText(Sudo_Id,0,"● اصدار سورس SEVEN \n● الاصدار » G.1 GOLD\n√")
+LuaTele.sendText(Sudo_Id,0,"● اصدار سورس SEVEN \n● الاصدار » G.3 GOLD\n√")
 dofile('NASA.lua')  
 return false 
 end
